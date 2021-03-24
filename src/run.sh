82406fd1de11838746204df2af5332037f95b416
Siddharth Nayak <siddharthnayak98@gmail.com>
2021-03-24 20:41:37 +0530

    run exps

--- src/run.sh
+++ src/run.sh
@@ -11,8 +11,9 @@ source /etc/profile
 module load anaconda/2020a 
 
 mkdir -p wandb_gnn
+mkdir -p out_files
 # Run the script
 nodes=(200 300 400 500 600 200 300 400 500 600 200 300 400 500 600)
 Ks=(2 2 2 2 2 3 3 3 3 3 5 5 5 5 5)
 # script to iterate through different hyperparameters
-python main.py --num_nodes=${nodes[$SLURM_ARRAY_TASK_ID]} --K=${Ks[$SLURM_ARRAY_TASK_ID]} --dataset='CIFAR' --batch_size=64 &> out_${nodes[$SLURM_ARRAY_TASK_ID]}_${Ks[$SLURM_ARRAY_TASK_ID]}
+python main.py --num_nodes=${nodes[$SLURM_ARRAY_TASK_ID]} --K=${Ks[$SLURM_ARRAY_TASK_ID]} --dataset='CIFAR' --batch_size=64 &> out_files/out_${nodes[$SLURM_ARRAY_TASK_ID]}_${Ks[$SLURM_ARRAY_TASK_ID]}_CIFAR
--- src/run_temp.sh
+++ src/run_temp.sh
@@ -11,8 +11,9 @@ source /etc/profile
 module load anaconda/2020a 
 
 mkdir -p wandb_gnn
+mkdir -p out_files
 # Run the script
 nodes=(500 500 500 600 600 600 700 700 700)
 Ks=(2 3 5 2 3 5 2 3 5)
 # script to iterate through different hyperparameters
-python main.py --num_nodes=${nodes[$SLURM_ARRAY_TASK_ID]} --K=${Ks[$SLURM_ARRAY_TASK_ID]} --remove_9='False' --batch_size=64 &> out_${nodes[$SLURM_ARRAY_TASK_ID]}_${Ks[$SLURM_ARRAY_TASK_ID]}
+python main.py --num_nodes=${nodes[$SLURM_ARRAY_TASK_ID]} --K=${Ks[$SLURM_ARRAY_TASK_ID]} --remove_9='False' --batch_size=64 &> out_files/out_${nodes[$SLURM_ARRAY_TASK_ID]}_${Ks[$SLURM_ARRAY_TASK_ID]}_MNIST
